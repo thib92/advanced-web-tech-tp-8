795b28de8285aacdf390646b27ea571f2a275c97
thib92 <thibhenry92@gmail.com>
2019-11-25 11:41:41 +0100

    Add babelrc and connect to database on server start

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import 'regenerator-runtime/runtime';
 
 import app from './app';
+import { getCitizenDal } from './citizenDal';
 
 const port = 8081;
 
-const server = app.listen(port, () => {
+const server = app.listen(port, async () => {
   const port = server.address().port;
   console.log('Server listening on port ' + port + '...');
+  await getCitizenDal().connect();
+  console.log('Database connected...');
 });
